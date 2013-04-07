1e7db1fa8372e15c18b6571a3553c6b08348b888
Richard Knop <richard.knop@saffrondigital.com>
2013-04-07 12:53:00 +0100

    started adding new view controller for filters

---
+++ GeekOut/FiltersController.h
@@ -0,0 +1,13 @@
+//
+//  FiltersController.h
+//  GeekOut
+//
+//  Created by Richard Knop on 07/04/2013.
+//  Copyright (c) 2013 Richard Knop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FiltersController : UIViewController
+
+@end
---
+++ GeekOut/FiltersController.mm
@@ -0,0 +1,38 @@
+//
+//  FiltersController.m
+//  GeekOut
+//
+//  Created by Richard Knop on 07/04/2013.
+//  Copyright (c) 2013 Richard Knop. All rights reserved.
+//
+
+#import "FiltersController.h"
+
+@interface FiltersController ()
+
+@end
+
+@implementation FiltersController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
